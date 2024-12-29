9d9bda21240ed6c8731dcf043209037fc66be75b
sama <samashalabi2018@gmail.com>
2024-12-29 17:19:08 +0200

    sending procecced data to kafka topic send_consumer_data

--- build.sbt
+++ build.sbt
@@ -5,36 +5,28 @@ ThisBuild / scalaVersion := "2.12.8"
 lazy val root = (project in file("."))
   .settings(
     name := "tweets processing",
-      libraryDependencies ++= Seq(
-          // https://mvnrepository.com/artifact/org.apache.spark/spark-core
-          "org.apache.spark" %% "spark-core" % "3.0.3",
-          // https://mvnrepository.com/artifact/org.apache.spark/spark-streaming
-          "org.apache.spark" %% "spark-streaming" % "3.0.3" ,
-          // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
-          "org.apache.kafka" % "kafka-clients" % "3.8.1",
-          // https://mvnrepository.com/artifact/org.apache.spark/spark-streaming-kafka-0-10
-          "org.apache.spark" %% "spark-streaming-kafka-0-10" % "3.0.3",
-          "org.json" % "json" % "20230618",
+    libraryDependencies ++= Seq(
+      // https://mvnrepository.com/artifact/org.apache.spark/spark-core
+      "org.apache.spark" %% "spark-core" % "3.0.3",
+      // https://mvnrepository.com/artifact/org.apache.spark/spark-streaming
+      "org.apache.spark" %% "spark-streaming" % "3.0.3",
+      // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
+      "org.apache.kafka" % "kafka-clients" % "3.8.1",
+      // https://mvnrepository.com/artifact/org.apache.spark/spark-streaming-kafka-0-10
+      "org.apache.spark" %% "spark-streaming-kafka-0-10" % "3.0.3",
+      "org.json" % "json" % "20230618",
 
-          "com.typesafe.play" %% "play-json" % "2.9.4",
-          "com.fasterxml.jackson.core" % "jackson-databind" % "2.15.2",
-          "com.johnsnowlabs.nlp" %% "spark-nlp" % "5.0.0",
+      "com.fasterxml.jackson.core" % "jackson-databind" % "2.15.2",
 
-          "org.tensorflow" % "tensorflow-core-platform" % "0.4.0",
-          "io.circe" %% "circe-core" % "0.14.6",
-          "io.circe" %% "circe-generic" % "0.14.6",
-          "io.circe" %% "circe-parser" % "0.14.6",
-          "org.apache.spark" %% "spark-sql" % "3.3.1",
-          "org.apache.spark" %% "spark-mllib" % "3.5.0",
+      "org.apache.spark" %% "spark-sql" % "3.0.3",
+      "org.apache.spark" %% "spark-mllib" % "3.5.0",
 
-          "com.johnsnowlabs.nlp" %% "spark-nlp-silicon" % "5.5.0",
-          "edu.stanford.nlp" % "stanford-corenlp" % "4.5.1",
+      "edu.stanford.nlp" % "stanford-corenlp" % "4.5.1",
 
-          "org.jfree" % "jfreechart" % "1.5.3",
-          "com.softwaremill.sttp.client3" %% "core" % "3.8.3",
-          "com.johnsnowlabs.nlp" %% "spark-nlp" % "5.0.0" % "provided" ,
-          "org.apache.poi" % "poi-ooxml" % "5.2.3"),
-
-      dependencyOverrides += "com.github.luben" % "zstd-jni" % "1.5.6-4"
+      "org.jfree" % "jfreechart" % "1.5.3",
+      "com.softwaremill.sttp.client3" %% "core" % "3.8.3",
+      "org.apache.poi" % "poi-ooxml" % "5.2.3"
+    ),
+    dependencyOverrides += "com.github.luben" % "zstd-jni" % "1.5.6-4"
 
   )
--- src/main/scala/streaming_data_processing.scala
+++ src/main/scala/streaming_data_processing.scala
@@ -1,17 +1,14 @@
-import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-
-import org.apache.spark.sql.{ SparkSession}
-import org.apache.spark.sql.functions._
-
-
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
 import java.io.File
 import scala.collection.mutable
+import java.util.Properties
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
@@ -23,7 +20,40 @@ case class TweetInfo(id: String, text: String, hashtags: Seq[String], timestamp:
 
 object streaming_data_processing {
   def main(args: Array[String]): Unit = {
-    val filePath = "C:\\Users\\Admin\\Desktop\\pro_big_scala\\Copy of Positive and Negative Word List.xlsx"
+    //    configuration for kafka
+    val brokers = "localhost:9092"
+    val groupId = "GRP1"
+    val topics = "produced_data" // multiple topics (separated by comma)
+
+    val SparkConf = new SparkConf().setMaster("local[*]").setAppName("streaming_data_processing")
+    val ssc = new StreamingContext(SparkConf, Seconds(10))
+    val sc = ssc.sparkContext
+    sc.setLogLevel("ERROR")
+    val spark = SparkSession.builder().config(SparkConf).getOrCreate()
+    import spark.implicits._
+
+    val topicSet = topics.split(",").toSet
+    val kafkaParams = Map[String, Object](
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> brokers,
+      ConsumerConfig.GROUP_ID_CONFIG -> groupId,
+      ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
+      ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer]
+    )
+
+    val messages = KafkaUtils.createDirectStream[String, String](
+      ssc, LocationStrategies.PreferConsistent, ConsumerStrategies.Subscribe[String, String](topicSet, kafkaParams)
+    )
+
+    // Kafka Producer Configuration
+    val kafkaProducerProps = new Properties()
+    kafkaProducerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    kafkaProducerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+    kafkaProducerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+
+    val producer = new KafkaProducer[String, String](kafkaProducerProps)
+
+    // preparations for sentiment functionality
+    val filePath = "Copy of Positive and Negative Word List.xlsx"
 
     val workbook = new XSSFWorkbook(new File(filePath))
 
@@ -44,36 +74,6 @@ object streaming_data_processing {
       }
     }
 
-
-
-    //    configuration for kafka
-    val brokers = "localhost:9092"
-    val groupId = "GRP1"
-    val topics = "my-topic" // multiple topics (separated by comma)
-
-    val SparkConf = new SparkConf().setMaster("local[*]").setAppName("streaming_data_processing")
-    val ssc = new StreamingContext(SparkConf, Seconds(10))
-    val sc = ssc.sparkContext
-    sc.setLogLevel("OFF")
-    // To avoid logging too many debugging messages
-    sc.setLogLevel("ERROR")
-    val spark = SparkSession.builder().config(SparkConf).getOrCreate()
-    import spark.implicits._
-
-    val topicSet = topics.split(",").toSet
-    val kafkaParams = Map[String, Object](
-      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> brokers,
-      ConsumerConfig.GROUP_ID_CONFIG -> groupId,
-      ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
-      ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer]
-    )
-
-    val messages = KafkaUtils.createDirectStream[String, String](
-      ssc, LocationStrategies.PreferConsistent, ConsumerStrategies.Subscribe[String, String](topicSet, kafkaParams)
-    )
-
-    // Extract JSON values from Kafka messages
-    val jsonStream = messages.map(record => record.value())
     val sentimentUDF = udf((text: String) => {
       val words = text.toLowerCase.split("\\W+")
       val goodScore = words.count(word => goodWordsSet.contains(word))
@@ -83,43 +83,45 @@ object streaming_data_processing {
       else if (badScore > goodScore) "Negative"
       else "Neutral"
     })
+
+    // Extract JSON values from Kafka messages
+    val jsonStream = messages.map(record => record.value())
     // Process each RDD in the DStream
     jsonStream.foreachRDD { rdd =>
       if (!rdd.isEmpty()) {
         // Convert RDD[String] to DataFrame
         val df = spark.read.json(rdd)
 
-        //        df.show(truncate = false)
         val wanted_data = df.select("tweet_id", "text", "hashtags", "lat", "lon", "created_at", "user_id", "user_name")
-        //        wanted_data.show(truncate = false)
+
+        // replace null values
         val cleaned_df = wanted_data.na.fill("N/A", Seq("tweet_id", "text", "hashtags", "created_at", "user_id", "user_name")).na.fill(0.0f, Seq("lat", "lon"))
-        //        cleaned_df.show(truncate = false)
+
+        // create list of hashtags
         val splitDF = cleaned_df.withColumn("hashtags", split(col("hashtags"), ","))
-        //        splitDF.show(false)
-         val secall =splitDF.withColumn("sentiment", sentimentUDF(col("text")))
 
+        // add sentiment column
+        val sentimentDF = splitDF.withColumn("sentiment", sentimentUDF(col("text")))
 
-        val nestedDF = secall.map(r => TweetInfo(r.getString(0), r.getString(1), r.getAs[Seq[String]](2), r.getString(5), coordinates(r.getDouble(3), r.getDouble(4)),r.getString(8), user_info(r.getString(6), r.getString(7))))
-        //        nestedDF.show(false)
+        // create json format
+        val JSONformat = sentimentDF.map(r => TweetInfo(r.getString(0), r.getString(1), r.getAs[Seq[String]](2), r.getString(5), coordinates(r.getDouble(3), r.getDouble(4)), r.getString(8), user_info(r.getString(6), r.getString(7))))
 
-        // Convert DataFrame to JSON strings
-        val jsonDataset = nestedDF.toJSON
-
-        // Collect JSON strings to driver
-        val jsonArray = jsonDataset.collect()
-
-        // Combine into a single JSON array string
-        val jsonString = jsonArray.mkString("[", ",", "]")
-
-        // Output the JSON string
-              println(jsonString)
-
+        // convert dataframe to json then sen it to kafka
+        JSONformat.toJSON.collect().foreach { jsonRecord =>
+          val record = new ProducerRecord[String, String]("send_consumer_data", null, jsonRecord)
+          producer.send(record)
+        }
       }
+      else println("no data received")
+    }
+
+    sys.addShutdownHook {
+      producer.close()
     }
 
     //**
     ssc.start()
     ssc.awaitTermination()
-    //
+
   }
 }
